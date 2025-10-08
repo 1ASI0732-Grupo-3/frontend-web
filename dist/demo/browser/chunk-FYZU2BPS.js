029699b0dc164e4222003323ef301865d5447bb2
Ian Macavilca Quispe <129208479+IanMQ@users.noreply.github.com>
2025-10-07 20:54:26 -0500

    deploy_v6

---
+++ dist/demo/browser/chunk-FYZU2BPS.js
@@ -0,0 +1,1 @@
+var E=function(e){return e.ACTIVE="active",e.COMPLETED="completed",e.SCHEDULED="scheduled",e.CANCELLED="cancelled",e}(E||{});export{E as a};
---
+++ dist/demo/browser/chunk-UX2XIPUK.js
@@ -0,0 +1,1 @@
+var E=function(n){return n.MEDICINES="Medicines",n.VACCINES="Vaccines",n.FOOD="Food",n.SUPPLEMENTS="Supplements",n}(E||{});export{E as a};
